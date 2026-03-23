e9d721c5993b76e1d924114492b6ee7bef4e902b
XXXDimMelXXX <melnikoffdd13@gmail.com>
2026-03-23 16:29:32 +0300

    third -commit with back

--- libs/shared-kernel/src/value-object.ts
+++
@@ -1,7 +0,0 @@
-export abstract class ValueObject<Props> {
-  protected constructor(protected readonly props: Props) {}
-
-  equals(other: ValueObject<Props>): boolean {
-    return JSON.stringify(this.props) === JSON.stringify(other.props);
-  }
-}
